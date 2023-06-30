1265442061260c93b1bb368f7f8b6f747bca313c
123-YUYUYU <1994847808@qq.com>
2023-06-30 16:33:51 +0800

    enter

---
+++ shell/dateservice.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+#chkconfig: 35 66 88
+#description:auto_run
+
+# 此脚本为介绍的第二种开机自启动脚本，即直接把脚本添加至系统执行文件中
+ntpdate cn.ntp.org.cn
---
+++ shell/initdate.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# 此脚本为介绍的第一种开机自启动脚本，用作rc.local下执行的脚本
+ntpdate cn.ntp.org.cn
